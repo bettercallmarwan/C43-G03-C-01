b1010e79bfd6ceddaae301e6a1b3ac45eb224562
marwan <marwanossama2004@gmail.com>
2024-12-12 21:21:49 +0200

    Final commit

--- project1/Program.cs
+++ project1/Program.cs
@@ -5,7 +5,38 @@
         // Entry point
         static void Main(string[] args)
         {
+            #region Welcome to csharp
+            // Display welcome messages
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello marwan");
+            Console.ReadLine(); // Read input from console
+            #endregion
+
+            #region Comments
+            // This is a single line comment
+
+            /*this is
+             a multi line
+            comment
+            */
+            #endregion
+
+
+            #region naming conventions
+
+            // Examples of naming conventions:
+
+            // Camel case:
+            //marwanOssama (Used in js) 
+
+            // Pascal case
+            //MarwanOssama (Used in c#)
+
+            //Snake Case
+            //marwan_ossama (Used in python and Angular)
+
+
+            #endregion
         }
     }
 }
